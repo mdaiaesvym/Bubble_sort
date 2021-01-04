b0067dffe3f5bb6bd4e0ba299b861b2c864eecf0
mdaiaesvym <akihirokanaya13@icloud.com>
2021-01-04 18:08:05 +0900

    Selection_sort

---
+++ Selection_sort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Selection_sort {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] list = {10,84,97,58,17,28,51,64,24,3};
+		int min,tmp = 0;
+
+		System.out.println("befor:" + Arrays.toString(list));
+
+		for(int i = 0; i < list.length - 1;i++) {
+			min = i;
+			for(int j = i + 1;j < list.length;j++) {
+				if(list[min] > list[j]) {
+					min = j;
+				}
+			}
+			tmp = list[i];
+			list[i] = list[min];
+			list[min] = tmp;
+		}
+
+		System.out.println("after:" + Arrays.toString(list));
+	}
+
+}
